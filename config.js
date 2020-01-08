ebc139e4473ee0eba2c94b708dafb0d395526a60
Yutao Chen <yutaochen95@gmail.com>
2020-01-07 18:56:44 -0600

    change zoomlevel

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var driveTime = driveSlides*driveSmoothness;
 var followPoint = true;
 
 // ...If so, what zoom, pitch, and bearing should be used to follow?
-var followZoomLevel = 90;
+var followZoomLevel = 30;
 var followBearing = 29;
 var followPitch = 0;
 
